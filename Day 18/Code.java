d6928a9445d3bf09b66b95d1e445b2f68aadb270
Shanvi <81086303+shanvii@users.noreply.github.com>
2023-12-02 15:49:04 +0530

    Update Code.java

--- Day 18/Code.java
+++ Day 18/Code.java
@@ -1,3 +1,27 @@
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.isRepresentingBST(arr, N));
+        }
+    }
+}
+
 class Solution {
     int prev = Integer.MIN_VALUE;
     int isRepresentingBST(int arr[], int N) {
