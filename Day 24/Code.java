fcbb458d94ce63c879c01eb00ad362d6db2c4b56
Shanvi <81086303+shanvii@users.noreply.github.com>
2023-12-08 18:23:58 +0530

    Create Code.java

---
+++ Day 24/Code.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcases = Integer.parseInt(br.readLine());
+        while (testcases-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(a1[i]);
+            }
+            Solution ob = new Solution();
+            int ans=ob.minNumber(a,n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int minNumber(int arr[], int N) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+
+        // Check if the sum is already prime
+        if (isPrime(sum)) {
+            return 0;
+        }
+
+        
+        for (int i = 1; ; i++) {
+            if (isPrime(sum + i)) {
+                return i;
+            }
+        }
+    }
+
+    
+    private boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
