2f2b5c925ce4442b62433ea0e86c13102bd9cac7
Shanvi <81086303+shanvii@users.noreply.github.com>
2023-12-11 20:14:30 +0530

    Create Code.java

---
+++ Day 27/Code.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            int N=sc.nextInt();
+            int K=sc.nextInt();
+            ArrayList<Integer> Arr = new ArrayList<Integer>(N); 
+  
+            // Appending new elements at 
+            // the end of the list 
+            for (int i = 0; i < N; i++) {
+                int x = sc.nextInt();
+                Arr.add(x); 
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.maximumSumSubarray(K,Arr,N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr, int N) {
+        long maxSum = 0;
+        long currentSum = 0;
+
+        // Calculate sum of first subarray of size K
+        for (int i = 0; i < K; i++) {
+            currentSum += Arr.get(i);
+        }
+
+        maxSum = currentSum;
+
+        // Slide the window to find maximum sum
+        for (int i = K; i < N; i++) {
+            currentSum = currentSum + Arr.get(i) - Arr.get(i - K);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
