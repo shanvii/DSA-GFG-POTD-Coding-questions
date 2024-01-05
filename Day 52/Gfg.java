b356df2b446d7cab7e1f0fddb58cd319909217d8
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-01-05 20:20:10 +0530

    Create Gfg.java

---
+++ Day 52/Gfg.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int N = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.TotalWays(N);
+            System.out.println(ans);           
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+   public int TotalWays(int N){
+       int mod=1000000007;
+       long[]dp=new long[N+1];
+       dp[0]=1;
+       dp[1]=2;
+       for(int i = 2; i<=N;i++)
+       dp[i]=(dp[i-1]+dp[i-2])%mod;
+       return(int)((dp[N]*dp[N])%mod);
+       
+   }
+   
+}
