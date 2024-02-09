d3d980c5245cfbd1e07c0a2642b0bdd0dbcb3e30
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-02-09 12:29:14 +0530

    Create GFG.java

---
+++ Day 87/GFG.java
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+//Initial Template for Java
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+ 
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+ 
+        inOrder(node.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+			    Solution g=new Solution();
+                System.out.println(g.isSumProperty(root));
+    	        
+	        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+
+/*Complete the function below
+Node is as follows:
+class Node{
+	int data;
+	Node left,right;
+	
+	Node(int key)
+	{
+	    data = key;
+	    left = right = null;
+	}
+}
+
+*/
+class Solution
+{
+    //Function to check whether all nodes of a tree have the value 
+    //equal to the sum of their child nodes.
+    public static int isSumProperty(Node root)
+    {
+        // add your code here
+        // Base case: If the tree is empty or it's a leaf node,
+        // then it satisfies the property.
+        if (root == null || (root.left == null && root.right == null))
+            return 1;
+        
+        int left_data = 0, right_data = 0;
+        
+        // If left child is not null, then get its data
+        if (root.left != null)
+            left_data = root.left.data;
+        
+        // If right child is not null, then get its data
+        if (root.right != null)
+            right_data = root.right.data;
+        
+        // Check current node's value with the sum of left and right children
+        // And recursively check for left and right subtrees
+        if ((root.data == left_data + right_data) 
+            && (isSumProperty(root.left) != 0) 
+            && (isSumProperty(root.right) != 0))
+            return 1;
+        else
+            return 0;
+    }
+}
